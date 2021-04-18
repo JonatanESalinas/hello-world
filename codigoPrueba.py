6eacf15cd8b810397728e7b4f07af4f2110d6185
JonatanESalinas <A01731815@itesm.mx>
2021-04-18 14:41:14 -0500

    Se agrego un print final y se quito 'res'.

--- codigoPrueba.py
+++ codigoPrueba.py
@@ -1,9 +1,9 @@
 
 
 def suma(num1, num2):
-	res = num1 + num2
-	return (res)
+	return (num1 + num2)
 
 if __name__ == '__main__':
 	print("Estoy usando GitHub")
 	print("Suma: " + str(suma(4,8)))
+	print("Se acabaron los calculos!")
