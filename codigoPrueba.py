297212e727ab1fb2a6aa1e2e39b6e203ca15b91f
JonatanESalinas <A01731815@itesm.mx>
2021-04-18 13:56:48 -0500

    Numbers are gotten with input()
    The user gives the numbers for the calculations.

--- codigoPrueba.py
+++ codigoPrueba.py
@@ -8,5 +8,7 @@ def my_calculation(num1, num2, modo):
 
 if __name__ == '__main__':
 	print("Estoy usando GitHub")
-	print("Suma: " + str(my_calculation(4,8,1)))
-	print("Multiplicacion: " + str(my_calculation(4,8,2)))
+	number1 = int(input("Please give me a number:\n"))
+	number2 = int(input("Please give me another number:\n"))
+	print("Suma: " + str(my_calculation(number1,number2,1)))
+	print("Multiplicacion: " + str(my_calculation(number1,number2,2)))
